7f9f26bdcc5cfc8929ea8f8b44fb26b1e1c23a87
rostik1406 <98974380+rostik1406@users.noreply.github.com>
2022-11-30 19:12:41 +0200

    .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,14 @@
-{
-  /* <iframe
-  id="vimeo-player"
-  src="https://player.vimeo.com/video/236203659"
-  width="640"
-  height="360"
-  frameborder="0"
-  allowfullscreen
-  allow="autoplay; encrypted-media"
-></iframe>; */
-}
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }),
+  1000
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const KEY_LOCALSTORAGE = 'feedback-form-state';
+const formData = {};
+
+const refs = {
+  formBox: document.querySelector('.feedback-form'),
+};
+
+refs.formBox.addEventListener('submit', onFormSubmit);
+refs.formBox.addEventListener('input', throttle(onFormInput, 500));
+
+populateForm();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const formDataToSend = new FormData(e.currentTarget);
+  formDataToSend.forEach((value, name) => {
+    formData[name] = value;
+  });
+  e.currentTarget.reset();
+  localStorage.removeItem(KEY_LOCALSTORAGE);
+  console.log(formData);
+}
+
+function onFormInput(e) {
+  formData.email = refs.formBox.email.value;
+  formData.message = refs.formBox.message.value;
+  localStorage.setItem(KEY_LOCALSTORAGE, JSON.stringify(formData));
+}
+
+function populateForm() {
+  const savedMessage = localStorage.getItem(KEY_LOCALSTORAGE);
+
+  if (savedMessage) {
+    const { email, message } = JSON.parse(savedMessage);
+
+    refs.formBox.email.value = email;
+    refs.formBox.message.value = message;
+    formData.email = email;
+    formData.message = message;
+  }
+}
