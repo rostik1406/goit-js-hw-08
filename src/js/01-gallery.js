d30a2b26b7f0567214a41877528b22c5edd91fc2
rostik1406 <98974380+rostik1406@users.noreply.github.com>
2022-12-07 19:53:03 +0200

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,43 +1,76 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-console.log(galleryItems);
 
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-//
-const gallery = document.querySelector('.gallery');
-const galleryList = createGalleryCardsItem(galleryItems);
-gallery.insertAdjacentHTML('beforeend', galleryList);
 
-function createGalleryCardsItem(galleryItems) {
-  return galleryItems
+const galleryList = document.querySelector('.gallery')
+
+const cardsMarkup = galleryItems
     .map(({ preview, original, description }) => {
-      return `
-    <div class="gallery__item">
-    <a class="gallery__link" href="${original}">
-    <img
-      loading="lazy" width="100%" height="100%"
-      class="gallery__img"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-    </a>
-    </div>
-    `;
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+        </a>
+        </div>
+        `
     })
     .join('');
+
+
+galleryList.insertAdjacentHTML('afterbegin', cardsMarkup)
+galleryList.addEventListener('click', onGalleryListClick)
+
+
+function onGalleryListClick(e) {
+    e.preventDefault()
+
+    if (!e.target.classList.contains('gallery__image')) return;
+    imageModalClick();
+
 }
 
-const addGallary = createGalleryCardsItem(galleryItems);
-
-const galleryFoto = document.querySelector('.gallery');
-
-galleryFoto.insertAdjacentHTML('beforeend', addGallary);
-
-const box = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionDelay: 200,
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
 });
+
+
+//     const url = getOriginalImgUrl(e); 
+//     instance = createModal(url);
+
+//     instance.show(window.addEventListener('keydown', onEscClick))
+
+// };
+
+// function getOriginalImgUrl(e) {
+//     return e.target.dataset.source;
+// };
+
+// function createModal(url) {
+//     return basicLightbox.create(
+//         `<img src="${url}">`, {
+//             onClose: () => {
+//                 window.removeEventListener('keydown', onEscClick);
+//             }
+//         }
+//     );
+// };
+
+
+// function onEscClick(e) {
+//     if (e.code === 'Escape') {
+//         instance.close();
+//     }
+// };
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,11 @@
 import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 
+
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
 
-player.on(
-  'timeupdate',
-  throttle(function (data) {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
-  }),
-  1000
-);
+player.on('timeupdate', throttle(function(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds)
+}), 1000);
